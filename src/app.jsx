8de556f1b4bfaf82699a4198848b7c0bbb9efb61
happykiller <fabrice.rosito@gmail.com>
2016-04-05 17:28:35 +0200

    update

--- src/app.jsx
+++ src/app.jsx
@@ -20,8 +20,17 @@ oda.createPoly({
 
 oda.createPoly({
     name: "oda-table",
+    param: {
+        id: {
+            value: "default"
+        },
+        list: {
+            value: []
+        }
+    },
     html: `<style>table, th, td {border: 1px solid black;}</style><table id="myTable"><thead><tr><th>col1</th></tr></thead><tbody></tbody></table>`,
     callback: (root, scope) => {
+        console.log(scope.list);
         let table = root.getElementById("myTable").getElementsByTagName('tbody')[0];
 
         // Create an empty <tr> element and add it to the 1st position of the table:
@@ -29,7 +38,12 @@ oda.createPoly({
 
         // Insert new cells (<td> elements) at the 1st and 2nd position of the "new" <tr> element:
         var cell1 = row.insertCell(0);
-        var cell2 = row.insertCell(1);
+
+        // Create an empty <tr> element and add it to the 1st position of the table:
+        var row = table.insertRow(1);
+
+        // Insert new cells (<td> elements) at the 1st and 2nd position of the "new" <tr> element:
+        var cell2 = row.insertCell(0);
 
         // Add some text to the new cells:
         cell1.innerHTML = "NEW CELL1";
